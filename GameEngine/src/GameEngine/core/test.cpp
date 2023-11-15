1fc81db4922fcb3022ec2194a01c9c491ff5e78f
type <type>
2023-11-14 16:19:25 -0800

    Fixed linker duplicated issue

---
+++ GameEngine/src/GameEngine/core/test.cpp
@@ -0,0 +1,6 @@
+#include <GameEngine/core/test.h>
+#include <Logger/core/Logger.h>
+
+void Test::msg(){
+    Logger::info("Information logged here!\n");
+}
---
+++ Sandbox/main.cpp
@@ -0,0 +1,7 @@
+#include <GameEngine/core/test.h>
+
+
+int main(){
+    Test t;
+    t.msg();
+}
