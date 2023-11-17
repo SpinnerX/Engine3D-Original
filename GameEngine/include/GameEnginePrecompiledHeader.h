7003636f395222d173921bf11b35ba467cfd2d5e
type <type>
2023-11-17 03:04:09 -0800

    Added precompiled headers for faster compilation and to not constantly recompile the same headers as well the original logging framework didnt work, so using spdlog instead

--- GameEngine/include/GameEnginePrecompiledHeader.h
+++ GameEngine/include/GameEnginePrecompiledHeader.h
@@ -11,3 +11,7 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+
+#if RENDER_PLATFORM_WINDOWS
+    #include <Windows.h> // include Windows.h if we are in Windows platform
+#endif
