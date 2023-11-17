ff6e9e759e71ff4a9dd2144126d50be1171df359
type <type>
2023-11-17 08:28:43 -0800

    Added lambdas for bound member function corresponding with a given argument

--- GameEngine/include/GameEngine/Application.cpp
+++ GameEngine/include/GameEngine/Application.cpp
@@ -35,14 +35,26 @@ namespace RendererEngine{
         // _window->setEventCallback(bind_event_function(onEvent));
 
         // _window->setEventCallback(bind_event_function(this, &Application::onEvent));
+
+        // Reasons not to use std::bind
         // Instead of using std::bind because of using more ram, and runtime, compile time overhead
         // using a lambda would be a better way to achhieve this
         // Usage:
         // we pass in an instance, and member function
         // As this lambda will accept that as the parameters
-        auto bind_function = [](auto* instance, auto member_function){
-            return [instance, member_function](auto&& arg1){
-                return (instance->*member_function)(std::forward<decltype(arg1)>(arg1));
+
+        // What is happening in this lambda?
+        // - This lambda creates another lambda that captures the instance and member function
+        // - From enclosing in this lambda. The new lambda which is a callable object and member function ptr
+        // - what we are returning is a dereferenced member function ptr of variable "member_function" on object instance
+        // - Then we std::forward arg1 which will just check here that if the object is a moved object or temp
+        //      then it becomes an rvalue
+        // - Though if object is persistent object then we use std::forward to decltype the arg1 getting actual type.
+        // - Which simply just means Invoking a bound member function with a given argument.
+        // - Invoking just means calling a function bounded by a class or is a specific objects method.
+        auto bind_function = [](auto* instance, auto M){
+            return [instance, M](auto&& arg1){
+                return (instance->*M)(std::forward<decltype(arg1)>(arg1));
             };
         };
         _window->setEventCallback(bind_function(this, &Application::onEvent));
