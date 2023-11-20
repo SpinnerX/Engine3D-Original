a0e2503d4f21da389c9f0ee4b301f817f9c6df6d
type <type>
2023-11-19 22:23:46 -0800

    Updated imgui cmake module

--- cmake/imgui.cmake
+++ cmake/imgui.cmake
@@ -16,6 +16,29 @@ if(EXISTS ${imgui_include})
         ${imgui_include}/imgui_tables.cpp
         ${imgui_include}/imgui_demo.cpp
         ${imgui_include}/imgui.cpp
+
+
+        # ${imgui_include}/backends/imgui_impl_opengl2.cpp
+        ${imgui_include}/backends/imgui_impl_glut.cpp
+        ${imgui_include}/backends/imgui_impl_glfw.cpp
+        # ${imgui_include}/backends/imgui_impl_dx9.cpp
+        # ${imgui_include}/backends/imgui_impl_dx12.cpp
+        # ${imgui_include}/backends/imgui_impl_dx11.cpp
+        # ${imgui_include}/backends/imgui_impl_dx10.cpp
+        # ${imgui_include}/backends/imgui_impl_android.cpp
+        # ${imgui_include}/backends/imgui_impl_allegro5.cpp
+
+        ${imgui_include}/backends/imgui_impl_opengl3.cpp
+        # ${imgui_include}/backends/imgui_impl_sdl2.cpp
+        # ${imgui_include}/backends/imgui_impl_sdl3.cpp
+        # ${imgui_include}/backends/imgui_impl_sdlrenderer2.cpp
+        # ${imgui_include}/backends/imgui_impl_sdlrenderer3.cpp
+        # ${imgui_include}/backends/imgui_impl_vulkan.cpp
+        # ${imgui_include}/backends/imgui_impl_wgpu.cpp
+        # ${imgui_include}/backends/imgui_impl_win32.cpp
+
+
+
     )
 else()
     message(SEND_ERROR "Imgui in /usr/local/include has not been found")
