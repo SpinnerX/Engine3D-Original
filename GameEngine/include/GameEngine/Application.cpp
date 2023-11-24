84a4ad9fb584fd5094deb79124e494ebc072e1c5
type <type>
2023-11-24 08:06:25 -0800

    SHADERS NOW WORK

--- GameEngine/include/GameEngine/Application.cpp
+++ GameEngine/include/GameEngine/Application.cpp
@@ -39,9 +39,6 @@ namespace RendererEngine{
         // Vertex Array
         // Vertex Buffer
         // Index Buffer
-        _vertexArr = 0;
-        _vertexBuffer = 0;
-        _indexBuffer = 0;
 
         // gen vertex array and bind them
         glGenVertexArrays(1, &_vertexArr);
@@ -77,6 +74,34 @@ namespace RendererEngine{
 
         unsigned int indices[3] = {0, 1, 2};
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+        std::string vertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+
+			out vec3 v_Position;
+
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = vec4(a_Position, 1.0);	
+			}
+		)";
+
+		std::string fragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+			}
+		)";
+
+        _shader.reset(new Shader(vertexSrc, fragmentSrc));
     }
 
     Application::~Application(){}
@@ -120,13 +145,14 @@ namespace RendererEngine{
 
     void Application::Run(){
         while(isRunning){
-            // glClearColor(1, 0, 1, 1);
-            // glClearColor(65,105,225, 1);
             glClearColor(0.1f, 0.1f, 0.1f, 1);
             glClear(GL_COLOR_BUFFER_BIT);
 
-            glBindVertexArray(_vertexArr);
-            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr); // called DrawElements because we are drawing indices
+            // glBindVertexArray(_vertexArr);
+            // glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr); // called DrawElements because we are drawing indices
+            _shader->bind();
+			glBindVertexArray(_vertexArr);
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
             for(Layer* layer : _layerStack){
                 layer->onUpdate();
