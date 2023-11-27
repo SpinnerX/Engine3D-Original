642fe5223c18d15bbf728d4cbef6eb08715634ae
type <type>
2023-11-27 10:44:38 -0800

    Moved actual application code into Sandbox

--- GameEngine/include/GameEngine/Application.cpp
+++ GameEngine/include/GameEngine/Application.cpp
@@ -1,12 +1,10 @@
 #include "Application.h"
 #include <GameEngine/platforms/Windows/WindowsInput.h>
-#include <GameEngine/platforms/OpenGL/OpenGLVertexArray.h>
-#include <GameEngine/Renderer/Renderer.h>
 
 namespace RendererEngine{
     Application* Application::_instance = nullptr;
     
-    Application::Application() : _camera(-1.6f, 1.6f, -0.9f, 0.9f){
+    Application::Application(){
 
         render_core_assert(!_instance, "Application already exists!");
         isRunning = true;
@@ -22,132 +20,6 @@ namespace RendererEngine{
         _imguiLayer = new ImGuiLayer();
         pushOverlay(_imguiLayer);
 
-        // gen vertex array and bind them
-        // glGenVertexArrays(1, &_vertexArr);
-        // glBindVertexArray(_vertexArr);
-        _vertexArray.reset(VertexArray::Create());
-        
-
-        // Now we are populating it with some data
-        // -0.5 is a quarter of the way from left side of the screen
-        // 0.5f is the other (right) side of the window.
-        // {x, y, z}
-        float vertices[3 * 7] = {
-			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
-			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
-			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
-		};
-
-        // The goal - VertexBuffer::Create returns specific rendering API depending on specic platform dependent rendering API
-        // VertexBuffer buffer = Vertex::Buffer(sizeof(vertices), vertices, GL_STATIC_DRAW);
-        // buffer.bind();
-        std::shared_ptr<VertexBuffer> _vertexBuffer;
-        _vertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
-
-       BufferLayout bufLayout = {
-            {ShaderDataType::Float3, "a_Position", true},
-            {ShaderDataType::Float4, "a_Color", true},
-       };
-
-       _vertexBuffer->setLayout(bufLayout);
-       _vertexArray->addVertexBuffer(_vertexBuffer);
-
-        std::shared_ptr<IndexBuffer> _indexBuffer;
-        uint32_t indices[3] = {0, 1, 2};
-        _indexBuffer.reset(IndexBuffer::Create(indices,  sizeof(indices) / sizeof(uint32_t)));
-        _vertexArray->setIndexBuffer(_indexBuffer);
-        
-
-        //////////////////////////
-        // Square Vertex Data //
-        //////////////////////////
-
-        float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-             -0.75f,  0.75f, 0.0f,
-		};
-
-        _squareVertexArrays.reset(VertexArray::Create());
-        std::shared_ptr<VertexBuffer> squareVB;
-        squareVB.reset(VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
-
-        squareVB->setLayout({
-            {ShaderDataType::Float3, "a_Position", true},
-        });
-        _squareVertexArrays->addVertexBuffer(squareVB);
-
-        uint32_t squareIndices[6] = {0, 1, 2, 2, 3, 0};
-        std::shared_ptr<IndexBuffer> squareIB;
-        squareIB.reset(IndexBuffer::Create(squareIndices,  sizeof(squareIndices) / sizeof(uint32_t)));
-        _squareVertexArrays->setIndexBuffer(squareIB);
-
-        std::string vertexSrc = R"(
-			#version 330 core
-			
-            uniform mat4 u_ViewProjection;
-
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec4 a_Color;
-
-			out vec3 v_Position;
-			out vec4 v_Color;
-
-			void main()
-			{
-				v_Position = a_Position;
-				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
-			}
-		)";
-
-		std::string fragmentSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) out vec4 color;
-
-			in vec3 v_Position;
-			in vec4 v_Color;
-
-			void main()
-			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
-				color = v_Color;
-			}
-		)";
-
-        _shader  = std::make_shared<Shader>(vertexSrc, fragmentSrc);
-        std::string blueShaderVertexSrc = R"(
-			#version 330 core
-
-            uniform mat4 u_ViewProjection;
-
-			layout(location = 0) in vec3 a_Position;
-
-			out vec3 v_Position;
-
-			void main()
-			{
-				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
-			}
-		)";
-
-		std::string blueShaderFragmentSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) out vec4 color;
-
-			in vec3 v_Position;
-
-			void main()
-			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
-			}
-		)";
-
-        _blueShader = std::make_shared<Shader>(blueShaderVertexSrc, blueShaderFragmentSrc);
     }
 
     Application::~Application(){}
@@ -192,18 +64,6 @@ namespace RendererEngine{
     void Application::Run(){
 
         while(isRunning){
-            RendererCommand::setClearColor({0.1f, 0.1f, 0.1f, 1});
-            RendererCommand::clear();
-
-            _camera.setPosition({1.0f, 1.0f, 0.0f}); // {x, y, z} (Changing Camera Position)
-            _camera.setRotation(45.5f);
-
-            Renderer::beginScene(_camera); // BeginScene
-
-            Renderer::submit(_blueShader, _squareVertexArrays);
-            Renderer::submit(_shader, _vertexArray); // Submitting our  objects or even meshes (or geo meshes)
-
-            Renderer::endScene(); // EndScene
 
             for(Layer* layer : _layerStack){
                 layer->onUpdate();
