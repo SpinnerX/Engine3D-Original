e83ba4dc8307489cd807241108aa4c514e9e50b8
type <type>
2023-11-27 09:19:37 -0800

    Added a line for glfwWindowHint function

--- GameEngine/src/GameEngine/platforms/WindowsWindow.cpp
+++ GameEngine/src/GameEngine/platforms/WindowsWindow.cpp
@@ -50,6 +50,7 @@ namespace RendererEngine{
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
         _window = glfwCreateWindow((int)props.width, (int)props.height, _data.title.c_str(), nullptr, nullptr);
         
 
