04b588f1f5eecf66df5c726dd141b18fe61eebc3
type <type>
2023-11-27 12:24:10 -0800

    Added Timestep class to get more consistent timing

---
+++ GameEngine/include/GameEngine/Core/Timestep.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace RendererEngine{
+
+    // Timestep
+    // - Helper class
+    class Timestep{
+    public:
+        Timestep(float time = 0.f) : _time(time) {}
+
+        float getSeconds() const { return _time; }
+
+        float getMilliseconds() const { return _time * 1000.0f; }
+
+        // This just returns a _time that is converted into a float
+        // Basically allowing to use this as a float
+        operator float() { return _time; }
+
+
+    private:
+        float _time;
+    };
+};
