916ceb0d26ddf3e423cabeea8873f4aa885691e4
type <type>
2023-11-27 09:21:38 -0800

    Added Camera as param when we begin a new scene and updated the submit function by calling uploadUniformMat4 function

--- GameEngine/include/GameEngine/Renderer/Renderer.cpp
+++ GameEngine/include/GameEngine/Renderer/Renderer.cpp
@@ -2,16 +2,20 @@
 #include <GameEngine/Renderer/Renderer.h>
 
 namespace RendererEngine{
+    Renderer::SceneData* Renderer::_sceneData = new Renderer::SceneData;
 
-    void Renderer::beginScene(){
-
+    void Renderer::beginScene(OrthographicCamera& camera){
+        _sceneData->viewProjectionMatrix = camera.getViewProjectionMatrix();
     }
 
     void Renderer::endScene(){
 
     }
 
-    void Renderer::submit(const std::shared_ptr<VertexArray>& vertexArray){
+    void Renderer::submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray){
+        shader->bind();
+        shader->uploadUniformMat4("u_ViewProjection", _sceneData->viewProjectionMatrix);
+
         vertexArray->bind();
         RendererCommand::drawIndexed(vertexArray);
     }
