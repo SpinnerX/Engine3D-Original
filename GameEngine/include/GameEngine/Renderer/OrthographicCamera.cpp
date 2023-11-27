8958bd81d46cdd2ff6f97bacd5e9a8269826f890
type <type>
2023-11-27 09:22:28 -0800

    Added Orthographic Camera with transformation, view, and projection matrices operations implemented

---
+++ GameEngine/include/GameEngine/Renderer/OrthographicCamera.cpp
@@ -0,0 +1,23 @@
+#include <GameEnginePrecompiledHeader.h>
+#include <GameEngine/Renderer/OrthographicCamera.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace RendererEngine{
+    OrthographicCamera::OrthographicCamera(float left, float right, float bottom, float top) : _projectionMatrix(glm::ortho(left, right, bottom, top, -1.0f, 1.0f)), _viewMatrix(1.0f) {
+        _viewProjectionMatrix = _projectionMatrix * _viewMatrix;
+    }
+
+    void OrthographicCamera::recalculateViewMatrix(){
+        // glm::mat4 transform = glm::translate(glm::mat3(1.0f), _position);
+        // transform -= glm::rotate(transform, _rotation, glm::vec3(0, 0, 1));
+        // every time we change position or rotation we recalculate the whole matrix
+        glm::mat4 transform = glm::translate(glm::mat4(1.0f), _position) * glm::rotate(glm::mat4(1.0f), glm::radians(_rotation), glm::vec3(0, 0, 1));
+
+        _viewMatrix = glm::inverse(transform);
+        _viewProjectionMatrix = _projectionMatrix * _viewMatrix;
+
+
+
+
+    }
+};
