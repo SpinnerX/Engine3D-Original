81fe97cd62c7c9d3dbad9ccfa83d05d9d094ed7b
= <=>
2023-11-28 05:18:42 -0800

    Added transform matrix as third param when we submit a shader

--- GameEngine/include/GameEngine/Renderer/Renderer.cpp
+++ GameEngine/include/GameEngine/Renderer/Renderer.cpp
@@ -12,9 +12,10 @@ namespace RendererEngine{
 
     }
 
-    void Renderer::submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray){
+    void Renderer::submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform){
         shader->bind();
         shader->uploadUniformMat4("u_ViewProjection", _sceneData->viewProjectionMatrix);
+        shader->uploadUniformMat4("u_Transform", transform); // whenever we submit, def need to do this per object
 
         vertexArray->bind();
         RendererCommand::drawIndexed(vertexArray);
