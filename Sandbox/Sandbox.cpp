a91fcee63d195ff7e0522d429e269c66d5edec68
type <type>
2023-12-02 06:32:00 -0800

    Refactored Sandbox a bit and removed some repeated tested code because TEXTURES ARE NOW WORKING FROM HERE ONWARDS

--- Sandbox/Sandbox.cpp
+++ Sandbox/Sandbox.cpp
@@ -149,28 +149,14 @@ public:
 			}
 		)";
 
-		// std::string textureShaderFragmentSrc = R"(
-		// 	#version 330 core
-			
-		// 	layout(location = 0) out vec4 color;
-
-		// 	in vec2 v_TexCoord;
-
-        //     uniform sampler2D u_Texture;
-
-		// 	void main()
-		// 	{
-		// 		color = texture(u_Texture, v_TexCoord);
-		// 	}
-		// )";
-        std::string textureShaderFragmentSrc = R"(
+		std::string textureShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 
 			in vec2 v_TexCoord;
-			
-			uniform sampler2D u_Texture;
+
+            uniform sampler2D u_Texture;
 
 			void main()
 			{
