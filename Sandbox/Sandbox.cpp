5cbceecbffd442c6acc35fa5f7b01b9dc0e6229e
type <type>
2023-12-02 20:52:17 -0800

    Updated instead of reading in strings for textures, refactored sandbox to load .glsl files instead to make code readable and cleaner

--- Sandbox/Sandbox.cpp
+++ Sandbox/Sandbox.cpp
@@ -58,81 +58,11 @@ public:
         squareIB.reset(RendererEngine::IndexBuffer::Create(squareIndices,  sizeof(squareIndices) / sizeof(uint32_t)));
         _squareVertexArrays->setIndexBuffer(squareIB);
 
-        std::string vertexSrc = R"(
-			#version 330 core
-			
-            uniform mat4 u_ViewProjection;
-            uniform mat4 u_Transform;
+        _shader.reset(RendererEngine::Shader::CreateShader("../assets/shaders/texture1.glsl"));
 
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec4 a_Color;
+        _flatShader.reset(RendererEngine::Shader::CreateShader("../assets/shaders/texture2.glsl"));
 
-			out vec3 v_Position;
-			out vec4 v_Color;
-
-			void main()
-			{
-				v_Position = a_Position;
-				v_Color = a_Color;
-				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
-			}
-		)";
-
-		std::string fragmentSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) out vec4 color;
-
-			in vec3 v_Position;
-			in vec4 v_Color;
-
-			void main()
-			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
-				color = v_Color;
-			}
-		)";
-
-        // _shader  = std::make_shared<RendererEngine::Shader>(vertexSrc, fragmentSrc);
-        _shader.reset(RendererEngine::Shader::CreateShader(vertexSrc, fragmentSrc));
-        std::string blueShaderVertexSrc = R"(
-			#version 330 core
-
-            uniform mat4 u_ViewProjection;
-            uniform mat4 u_Transform;
-
-
-			layout(location = 0) in vec3 a_Position;
-
-			out vec3 v_Position;
-
-			void main()
-			{
-				v_Position = a_Position;
-				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
-			}
-		)";
-
-		std::string flatColorShaderFragmentSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) out vec4 color;
-
-			in vec3 v_Position;
-
-            uniform vec3 u_Color;
-
-			void main()
-			{
-				color = vec4(u_Color, 1.0);
-			}
-		)";
-
-        _flatShader.reset(RendererEngine::Shader::CreateShader(blueShaderVertexSrc, flatColorShaderFragmentSrc));
-
-        _textureShader.reset(RendererEngine::Shader::CreateShader("../assets/shaders/texture.glsl"));
-
-        // _textureShader.reset(RendererEngine::Shader::CreateShader(textureShaderVertexSrc, textureShaderFragmentSrc));
+        _textureShader.reset(RendererEngine::Shader::CreateShader("../assets/shaders/texture3.glsl"));
         
         _texture = RendererEngine::Texture2D::Create("../assets/Checkerboard.png");
         _textureWithAlpha = RendererEngine::Texture2D::Create("../assets/texture2.png");
