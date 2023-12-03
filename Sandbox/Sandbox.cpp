0ba4c62033f67bd0941bfa21f5e0a03f0ca9d75d
type <type>
2023-12-02 20:44:14 -0800

    Shaders Assets files for loading .glsl files FINALLY WORKS MOVING ONWARDS

--- Sandbox/Sandbox.cpp
+++ Sandbox/Sandbox.cpp
@@ -130,41 +130,9 @@ public:
 
         _flatShader.reset(RendererEngine::Shader::CreateShader(blueShaderVertexSrc, flatColorShaderFragmentSrc));
 
-        std::string textureShaderVertexSrc = R"(
-			#version 330 core
+        _textureShader.reset(RendererEngine::Shader::CreateShader("../assets/shaders/texture.glsl"));
 
-			layout(location = 0) in vec3 a_Position;
-            layout(location = 1) in vec2 a_TexCoord;
-            
-            uniform mat4 u_ViewProjection;
-            uniform mat4 u_Transform;
-
-			out vec2 v_TexCoord;
-
-
-			void main()
-			{
-                v_TexCoord = a_TexCoord;
-				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
-			}
-		)";
-
-		std::string textureShaderFragmentSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) out vec4 color;
-
-			in vec2 v_TexCoord;
-
-            uniform sampler2D u_Texture;
-
-			void main()
-			{
-				color = texture(u_Texture, v_TexCoord);
-			}
-		)";
-
-        _textureShader.reset(RendererEngine::Shader::CreateShader(textureShaderVertexSrc, textureShaderFragmentSrc));
+        // _textureShader.reset(RendererEngine::Shader::CreateShader(textureShaderVertexSrc, textureShaderFragmentSrc));
         
         _texture = RendererEngine::Texture2D::Create("../assets/Checkerboard.png");
         _textureWithAlpha = RendererEngine::Texture2D::Create("../assets/texture2.png");
