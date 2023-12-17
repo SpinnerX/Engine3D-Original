6b2b86b507b88ff1f236559b3516ea7a6a2b3348
type <type>
2023-12-17 00:06:08 -0800

    Fixed OrthographicCameraControllers and NOW ORTHO CAMERA CONTROLLERS WORK FROM HERE ONWARDS

--- GameEngine/include/GameEngine/OrthographicCameraController.cpp
+++ GameEngine/include/GameEngine/OrthographicCameraController.cpp
@@ -22,16 +22,24 @@ namespace RendererEngine{
         else if(InputPoll::isKeyPressed(RENDER_KEY_S)){ // DOWN
             _cameraPosition.y += _cameraTranslationSpeed * ts;
         }
-
-        if(InputPoll::isKeyPressed(RENDER_KEY_Q)){ // LEFT
-            _cameraRotation += _cameraRotationSpeed * ts;
-        }
-        else if(InputPoll::isKeyPressed(RENDER_KEY_E)){ // RIGHT
-            _cameraRotation -= _cameraRotationSpeed * ts;
-        }
 		
-		_camera.setRotation(_cameraRotation);
+		// Checking if we set the rotation speed
+		if(_rotation){
+
+			if(InputPoll::isKeyPressed(RENDER_KEY_Q)){ // Rotate Right
+				_cameraRotation -= _cameraRotationSpeed * ts;
+			}
+
+			if(InputPoll::isKeyPressed(RENDER_KEY_E)){ // Rotate Left
+				_cameraRotation += _cameraRotationSpeed * ts;
+			}
+			
+			// Then we set the camera rotation
+			_camera.setRotation(_cameraRotation);
+		}
+		
 		_camera.setPosition(_cameraPosition);
+		_cameraTranslationSpeed = _zoomLevel;
 	}
 
 	void OrthographicCameraController::onEvent(Event& e){
@@ -42,7 +50,10 @@ namespace RendererEngine{
 
 	
 	bool OrthographicCameraController::onMouseScrolled(MouseScrolledEvent& e){
-		_zoomLevel -= e.GetYOffset();
+		_zoomLevel -= e.GetYOffset() * 0.25f; // Modify thihs const offset for window resized event event
+		
+		// NOTE: When zooming too far out or too close, you eventually end up flipping your view upside down.
+		_zoomLevel = std::max(_zoomLevel, 0.25f); // This allows us to control our offsets for our zooming level, making sure we don't zoom too far our or zooming in too close.
 		_camera.setProjection(-_aspectRatio * _zoomLevel, _aspectRatio * _zoomLevel, -_zoomLevel, _zoomLevel);
 		return false;
 	}
