4b3fce63c192a0192ec72e06e08a4026d4ffb8de
type <type>
2024-01-02 19:37:49 -0800

    Did some maintenance and refactor include paths, by moving core includes into this code directory

---
+++ GameEngine/include/GameEngine/Core/Application.cpp
@@ -0,0 +1,104 @@
+#include "Application.h"
+#include "EngineLogger.h"
+#include "Events/ApplicationEvent.h"
+#include <GameEngine/Renderer/Renderer.h>
+#include <GameEngine/platforms/Windows/WindowsInput.h>
+
+namespace RendererEngine{
+    Application* Application::_instance = nullptr;
+    
+    Application::Application(){
+
+        render_core_assert(!_instance, "Application already exists!");
+        isRunning = true;
+		isMinimized = false;
+        _instance = this;
+        _window = std::unique_ptr<Window>(Window::create());
+
+        _window->setEventCallback(bind_function(this, &Application::onEvent));
+
+        // Initializing our renderer
+        Renderer::init();
+
+        _imguiLayer = new ImGuiLayer();
+        pushOverlay(_imguiLayer);
+
+    }
+
+    Application::~Application(){}
+
+    void Application::pushLayer(Layer* layer){
+        _layerStack.pushLayer(layer);
+        layer->onAttach();
+    }
+
+    void Application::pushOverlay(Layer* layer){
+        _layerStack.pushOverlay(layer);
+        layer->onAttach();
+    }
+
+
+    void Application::onEvent(Event& event){
+        EventDispatcher dispatcher(event);
+        // In order for dispatcher to tell which event to execute, this is where that happens
+
+        // NOTE
+        // - Dispatcher checks the incoming type event is the same as the static type in the Dispatch function
+        //  then we execute that specific callback corresponding to that event.
+        dispatcher.Dispatch<WindowCloseEvent>(bind_function(this, &Application::onWindowClose));
+        dispatcher.Dispatch<WindowResizeEvent>(bind_function(this, &Application::onWindowResize));
+
+        // Iterating backwards thhrough the layer stack and thhen we called onEvent, and if it isn;t handled thhen it breaks 
+        // If not a layer and an overlay then we do not continue.
+        auto iter = _layerStack.end();
+        while(iter != _layerStack.begin()){
+            (*--iter)->onEvent(event);
+
+            if(event._handled) break;
+        }
+
+    }
+
+
+    void Application::Run(){
+
+        while(isRunning){
+            // Is going to be showing how lonmg this frame current time and the last frame time
+            float time = (float)glfwGetTime(); // Should be in platform::getTime() (containing impl for Mac, Windows, etc.)
+            Timestep timestep = time - _lastFrameTime;
+            _lastFrameTime = time;
+			
+			if(!isMinimized){
+				for(Layer* layer : _layerStack){
+				    layer->onUpdate(timestep);
+				}
+
+				_imguiLayer->begin();
+				for(Layer* layer : _layerStack){
+				    layer->onImguiRender();
+				}
+				_imguiLayer->end();
+			}
+
+            _window->onUpdate();
+        }
+    }
+
+    bool Application::onWindowClose(WindowCloseEvent& e){
+        isRunning = false;
+        return true;
+    }
+
+	bool Application::onWindowResize(WindowResizeEvent& e){
+		if(e.GetWidth() == 0 || e.GetHeight() == 0){
+			isMinimized = true;
+			coreLogInfo("True said here!");
+			return false;
+		}
+
+		// Telling the renderer the frame buffer has been resized
+		isMinimized = false;
+		Renderer::onWindowResize(e.GetWidth(), e.GetHeight());
+		return false;
+	}
+};
---
+++ GameEngine/include/GameEngine/Core/Application.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "Events/ApplicationEvent.h"
+#include <memory>
+#include <GameEngine/Core/core.h>
+#include <GameEngine/Core/Window.h>
+#include <GameEngine/Core/LayerStack.h>
+#include <GameEngine/Imgui/ImGuiLayer.h>
+#include <GameEngine/Core/Timestep.h>
+
+namespace RendererEngine{
+    // Application
+    // - Essentially how users will run their own applications
+    class Application{
+    public:
+        Application();
+        ~Application();
+
+        void Run();
+
+        // We can use the EventDispatcher created to dispatch events depending on what kind of
+        // events that is needed to dispatched being passed in.
+        void onEvent(Event& event);
+
+        void pushLayer(Layer* layer);
+        void pushOverlay(Layer* layer);
+
+        // Wherever we are in our codebase, we will want to access the application
+        // since this application contain important informaiton
+        // Therefore why we only create one instance.
+        inline static Application& Get() { return *_instance; }
+        inline Window& GetWindow() { return *_window; }
+
+    private:
+        bool onWindowClose(WindowCloseEvent& e);
+        bool onWindowResize(WindowResizeEvent& e);
+
+    private:
+        std::unique_ptr<Window> _window; // a unique pointer because this class owns this ptr
+        ImGuiLayer* _imguiLayer; // ImGuiLayer class (this class owns this ptr)
+        bool isRunning;
+		bool isMinimized;
+        LayerStack _layerStack;
+
+        float _lastFrameTime = 0.0f; // Time it took to render the last frame
+    private:
+        // Since there really is only going to be one application
+        static Application* _instance; // Getting our current application instance
+    };
+
+    // To be defined in client.
+    Application* CreateApplication();
+};
