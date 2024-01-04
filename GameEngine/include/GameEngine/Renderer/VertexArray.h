a420c55d3a75544e8a9ced48cdb4e03a0488e775
type <type>
2024-01-04 02:08:56 -0800

    Updated Vertex Arrays by adding setViewport getters and setters

--- GameEngine/include/GameEngine/Renderer/VertexArray.h
+++ GameEngine/include/GameEngine/Renderer/VertexArray.h
@@ -28,7 +28,7 @@ namespace RendererEngine{
         virtual const std::vector<Ref<VertexBuffer>>& getVertexBuffer() const  = 0;
         virtual const Ref<IndexBuffer>& getIndexBuffer() const  = 0;
 
-        static VertexArray* Create();
+        static Ref<VertexArray> Create();
 
     };
 };
