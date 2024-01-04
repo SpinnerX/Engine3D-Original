7780bf4ff0c2e35585f07cb592ca42e46cf2b4c5
type <type>
2024-01-04 04:54:31 -0800

    Added basic functionality of 2D Renderer

---
+++ GameEngine/include/GameEngine/Renderer/Renderer2D.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Renderer/OrthographicCamera.h"
+
+namespace RendererEngine{
+
+	class Renderer2D{
+	public:	
+		static void Init();
+		static void Shutdown();
+
+		static void beginScene(const OrthographicCamera& camera);
+		static void endScene();
+
+		// primitives
+		
+		// TODO: Potentially use glm::vec3 to sort, by utilizing that z and modifying that value.
+		//		 Setting that z values
+		static void drawQuad(const glm::vec2& pos, const glm::vec2& size, const glm::vec4& color);
+		static void drawQuad(const glm::vec3& pos, const glm::vec2& size, const glm::vec4& color);
+	};
+
+};
