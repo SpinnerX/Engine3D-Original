9d7be86914facb844e10310b85ea1f25faa57b94
type <type>
2024-01-04 04:53:56 -0800

    Updated by adding renderer includes in GameEngine.h header

--- GameEngine/include/GameEngine/Core/GameEngine.h
+++ GameEngine/include/GameEngine/Core/GameEngine.h
@@ -25,12 +25,14 @@
 #include <GameEngine/Imgui/ImGuiLayer.h>
 
 // Renderer-related stuff (virtual interfaces, impl's, etc)
+#include <GameEngine/Renderer/Renderer.h>
+#include <GameEngine/Renderer/Renderer2D.h>
+#include <GameEngine/Renderer/RenderCommand.h>
+
 #include <GameEngine/Renderer/Buffer.h>
 #include <GameEngine/Renderer/Shader.h>
 #include <GameEngine/Renderer/Texture.h>
 #include <GameEngine/Renderer/VertexArray.h>
-
-#include <GameEngine/Renderer/RenderCommand.h>
 
 
 #include <GameEngine/Renderer/Renderer.h>
