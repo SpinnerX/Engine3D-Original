c3efabd7e50b40f24f227dae50b014e4e6645cba
type <type>
2024-01-05 00:12:25 -0800

    Updated Sandbox2D by adding more profilers

--- Sandbox/Sandbox2D.cpp
+++ Sandbox/Sandbox2D.cpp
@@ -1,51 +1,9 @@
 #include "Sandbox2D.h"
+#include "Debug/Instrumentor.h"
 #include "Renderer/Renderer2D.h"
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <GameEngine/platforms/OpenGL/OpenGLShader.h>
-
-// It would be better to use a lambda without constructing a std::function
-// Because a lambda will actually generate a type that is unique to this lambda, which is why there is a use for templates or auto to handle that
-template<typename Fn>
-class Timer{
-public:
-
-	Timer(const char* n, Fn&& function) : _name(n), _function(function), _stopped(false){
-		// Starting the Timer
-		_startTimePoint = std::chrono::high_resolution_clock::now();
-	}
-
-	~Timer(){
-		if(!_stopped){
-			stop();
-		}
-	}
-
-	void stop(){
-
-		// Recording out actual total time
-		auto endTimePoint = std::chrono::high_resolution_clock::now();
-
-		long long start = std::chrono::time_point_cast<std::chrono::microseconds>(_startTimePoint).time_since_epoch().count();
-		long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimePoint).time_since_epoch().count();
-		_stopped = true;
-		
-		float duration = (end - start) * 0.001f;
-		_function({_name, duration});
-	}
-
-private:
-	const char* _name;
-	Fn _function;
-	std::chrono::time_point<std::chrono::steady_clock> _startTimePoint;
-	bool _stopped;
-};
-
-// NOTE: WiLL Be changing this as this is temporary
-#define PROFILE_SCOPE(name) Timer timer##__LINE__(name, [&](ProfileResult profileResult) { _profileResults.push_back(profileResult); })
-/* static void start(const char* name){ */
-/* 	Timer timer(name [&](ProfileResult profileResult){ _profileResults.push_back(profileResult); }); */
-/* } */
 
 Sandbox2D::Sandbox2D() : Layer("Sandbox2D"), _cameraController(1280.0f / 720.0f){
 }
@@ -59,24 +17,24 @@ void Sandbox2D::onDetach(){
 
 void Sandbox2D::onUpdate(RendererEngine::Timestep ts){
 	/* Timer timer("Sandbox2D::onUpdate", [&](auto profileResult){ _profileResults.push_back(profileResult); }); */
-	PROFILE_SCOPE("Sandbox2D::onUpdate");
+	RENDER_PROFILE_FUNCTION();
 	
 	// Update
 	{
 		/* Timer timer("CameraController::onUpdate", [&](auto profileResult) { _profileResults.push_back(profileResult); }); */
-		PROFILE_SCOPE("CameraController::onUpdate");
+		RENDER_PROFILE_FUNCTION();
 		_cameraController.onUpdate(ts);
 	}
 
 	// Render
 	{
-		PROFILE_SCOPE("Renderer Prep");
+		RENDER_PROFILE_FUNCTION();
 		RendererEngine::RendererCommand::setClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 		RendererEngine::RendererCommand::clear();
 	}
 	
 	{
-		PROFILE_SCOPE("Renderer Draw");
+		RENDER_PROFILE_FUNCTION();
 		RendererEngine::Renderer2D::beginScene(_cameraController.getCamera());
 	
 		// Our draw quads, for drawing our rectangles.
@@ -92,20 +50,6 @@ void Sandbox2D::onImguiRender()
 {
 	ImGui::Begin("Settings");
 	ImGui::ColorEdit4("Square Color", glm::value_ptr(_squareColor));
-
-
-	for(auto& result : _profileResults){
-		char label[50];
-
-		/* strncpy(label, fmt::format("{:.3f}ms is {}", result.time, result.name).c_str(), sizeof(label) - 1); */
-		strcpy(label, fmt::format("{:.3f}ms is {}", result.time, result.name).c_str());
-		
-		ImGui::Text("%s", label);
-	}
-
-	_profileResults.clear();
-
-
 	ImGui::End();
 }
 
