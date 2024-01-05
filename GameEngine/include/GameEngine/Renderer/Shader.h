ca9b39b44468052ace8460995a3d77797cb1a935
type <type>
2024-01-05 03:41:19 -0800

    Updated Shader by adding setFloat function

--- GameEngine/include/GameEngine/Renderer/Shader.h
+++ GameEngine/include/GameEngine/Renderer/Shader.h
@@ -18,6 +18,9 @@ namespace RendererEngine{
         virtual void unbind() const = 0;
 		
 		virtual void setInt(const std::string& name, int value) = 0;
+		
+		virtual void setFloat(const std::string& name, float) = 0;
+		
 		virtual void setFloat3(const std::string& name, const glm::vec3& value) = 0;
 
 		virtual void setFloat4(const std::string& name, const glm::vec4& value) = 0;
