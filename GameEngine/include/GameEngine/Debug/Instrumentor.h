9699c247910eaabcfae7971cbabfa17d6775525c
type <type>
2024-01-06 00:43:47 -0800

    Added mutex to avoid jumbled output and race conditions if profiling with multiple threads, ensuring a newly beginSession will cleanly end any previous existing sessions. Also removed the std::hash<> for threadID

--- GameEngine/include/GameEngine/Debug/Instrumentor.h
+++ GameEngine/include/GameEngine/Debug/Instrumentor.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "Core/EngineLogger.h"
+#include <mutex>
 #include <string>
 #include <chrono>
 #include <algorithm>
@@ -25,7 +27,7 @@ namespace RendererEngine{
 	struct ProfileResult{
 		std::string name;
 		long long start, end;
-		uint32_t threadID;
+		std::thread::id threadID;
 	};
 
 	struct InstrumentationSession{
@@ -38,20 +40,36 @@ namespace RendererEngine{
 		Instrumentor() : _currentSession(nullptr), _profileCount(0) {}
 
 		void beginSession(const std::string& name, const std::string& filepath = "results.json"){
+			std::lock_guard<std::mutex> guard(_mutex);
+			
+			/*
+			 *
+			 * @beginSession
+			 * @note before starting a session if one session already exists, then close that session before starting a new one.
+			 * @note Profiling output means for original session will be the newly opened session instead (better then having it be badly formatted)
+			 * @note profiling output
+			 *
+			*/
+			if(_currentSession){
+				if(EngineLogger::GetCoreLogger()){ // We are checking just in case anothher session has already been initiated before EngineLogger has been initialized.
+					coreLogError("Instrumentor::BeginSession('{}') when session '{}' already open.", name, _currentSession->name);
+					internalEndSession();
+				}
+				
+			}
 			 _outputStream.open(filepath);
 			writeHeader();
 			_currentSession = new InstrumentationSession{ name };
 		}
 		
 		void endSession(){
-			writeFooter();
-			_outputStream.close();
-			delete _currentSession;
-			_currentSession = nullptr;
-			_profileCount = 0;
+			std::lock_guard<std::mutex> guard(_mutex);
+			internalEndSession();
 		}
 
 		void writeProfile(const ProfileResult& result){
+			std::stringstream json;
+
 			if (_profileCount++ > 0){
 				_outputStream << ",";
 			}
@@ -59,19 +77,29 @@ namespace RendererEngine{
 			std::string name = result.name;
 			std::replace(name.begin(), name.end(), '"', '\'');
 
-			_outputStream << "{";
-			_outputStream << "\"cat\":\"function\",";
-			_outputStream << "\"dur\":" << (result.end - result.start) << ',';
-			_outputStream << "\"name\":\"" << name << "\",";
-			_outputStream << "\"ph\":\"X\",";
-			_outputStream << "\"pid\":0,";
-			_outputStream << "\"tid\":" << result.threadID << ",";
-			_outputStream << "\"ts\":" << result.start;
-			_outputStream << "}";
-
-			_outputStream.flush();
+			json << "{";
+			json << "\"cat\":\"function\",";
+			json << "\"dur\":" << (result.end - result.start) << ',';
+			json << "\"name\":\"" << name << "\",";
+			json << "\"ph\":\"X\",";
+			json << "\"pid\":0,";
+			json << "\"tid\":" << result.threadID << ",";
+			json << "\"ts\":" << result.start;
+			json << "}";
+			
+			std::lock_guard<std::mutex> guard(_mutex);
+			if(_currentSession){
+				_outputStream << json.str();
+				_outputStream.flush();
+			}
 		}
 
+		static Instrumentor& get(){
+			static Instrumentor instance;
+			return instance;
+		}
+
+	private:
 		void writeHeader(){
 			_outputStream << "{\"otherData\": {},\"traceEvents\":[";
 			_outputStream.flush();
@@ -81,16 +109,22 @@ namespace RendererEngine{
 			_outputStream << "]}";
 			_outputStream.flush();
 		}
-
-		static Instrumentor& get(){
-			static Instrumentor instance;
-			return instance;
+		
+		// NOTE: Keep in mind that you must already own that mutex before
+		// calling on internalEndSession!
+		void internalEndSession(){
+			if(_currentSession){
+				writeFooter();
+				_outputStream.close();
+				delete _currentSession;
+				_currentSession = nullptr;
+			}
 		}
-
 	private:
 		InstrumentationSession* _currentSession;
 		std::ofstream _outputStream;
 		int _profileCount;
+		std::mutex _mutex;
 	};
 
 	class InstrumentorTimer{
@@ -112,7 +146,7 @@ namespace RendererEngine{
 			long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
 
 			uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-			Instrumentor::get().writeProfile({ _name, start, end, threadID });
+			Instrumentor::get().writeProfile({ _name, start, end, std::this_thread::get_id() });
 
 			_stopped = true;
 		}
