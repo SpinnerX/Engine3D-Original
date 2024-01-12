ea66cd75190ff79f3d1f134dea541675dc402a6f
type <type>
2024-01-12 04:58:45 -0800

    Updated Sandbox to utilizing drawRotatedQuads that now have rotation in the batch renderer, which worksss

--- Sandbox/Sandbox2D.cpp
+++ Sandbox/Sandbox2D.cpp
@@ -33,14 +33,16 @@ void Sandbox2D::onUpdate(RendererEngine::Timestep ts){
 	{
 		RENDER_PROFILE_FUNCTION();
 		RendererEngine::Renderer2D::beginScene(_cameraController.getCamera());
-	
+		static float rotation = 0.0f;
+		rotation += ts * 50.0f;
+
+
 		// Our draw quads, for drawing our rectangles.
-		/* RendererEngine::Renderer2D::drawRotatedQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, glm::radians(-45.0f), {0.8f, 0.2f, 0.3f, 1.0f}); // Red shape */
+		RendererEngine::Renderer2D::drawRotatedQuad({1.0f, 0.0f}, {0.8f, 0.8f}, -45.0f, {0.8f, 0.2f, 0.3f, 1.0f}); // Red shape
 		RendererEngine::Renderer2D::drawQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, {0.8f, 0.2f, 0.3f, 1.0f}); // Red shape
 		RendererEngine::Renderer2D::drawQuad({0.5f, -0.5f}, {0.5f, 0.75f}, {0.2f, 0.3f, 0.8f, 1.0f}); // blue shape
-		/* RendererEngine::Renderer2D::drawQuad({0.0f, 0.0f, -0.1f}, {10.0f, 10.0f}, _checkerboardTexture, 10.0f, glm::vec4(1.0f, 0.9f, 0.9f, 1.0f)); // checkerboard texture shape */
-		RendererEngine::Renderer2D::drawQuad({-5.0f, -5.0f, -0.1f}, {10.0f, 10.0f}, _checkerboardTexture, 10.0f); // checkerboard texture shape
-		RendererEngine::Renderer2D::drawQuad({ -0.5f, -0.5f, 0.0f}, {1.0f, 1.0f}, _checkerboardTexture, 20.0f); // checkerboard texture shape
+		RendererEngine::Renderer2D::drawQuad({0.0f, 0.0f, -0.1f}, {10.0f, 10.0f}, _checkerboardTexture, 10.0f); // checkerboard texture shape
+		RendererEngine::Renderer2D::drawRotatedQuad({ -2.0f, 0.0f, 0.0f}, {1.0f, 1.0f}, rotation, _checkerboardTexture, 20.0f); // checkerboard texture shape
 	
 		RendererEngine::Renderer2D::endScene();
 	}
