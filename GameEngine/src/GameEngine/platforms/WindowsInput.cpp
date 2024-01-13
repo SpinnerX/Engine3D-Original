44847e06ec974a725d0e1feefb6cc42c129d3412
type <type>
2024-01-12 23:13:26 -0800

    To use MouseCode and KeyCode enums that represent our different mouse and key codes

--- GameEngine/src/GameEngine/platforms/WindowsInput.cpp
+++ GameEngine/src/GameEngine/platforms/WindowsInput.cpp
@@ -2,23 +2,24 @@
 #include <GameEngine/Core/Application.h>
 #include <GameEngine/platforms/Windows/WindowsInput.h>
 #include <GLFW/glfw3.h>
+#include <GameEngine/Core/MouseButtonCodes.h>
 
 namespace RendererEngine{
     Scope<InputPoll> InputPoll::_instance = CreateScope<WindowsInput>();
 
-    bool WindowsInput::isKeyPressedImpl(int keycode){
+    bool WindowsInput::isKeyPressedImpl(KeyCode keycode){
         // This will call GLFW and do everything needed when a key has been pressed
         auto window = static_cast<GLFWwindow *>(Application::Get().GetWindow().getNativeWindow());
-        auto state = glfwGetKey(window, keycode);
+		auto state = glfwGetKey(window, static_cast<int32_t>(keycode));
 
         return state == GLFW_PRESS || state == GLFW_REPEAT;
     }
 
     // Handling mouse pressing events
     // and handling mouse cursor location
-    bool WindowsInput::isMouseButtonPressedImpl(int button) {
+    bool WindowsInput::isMouseButtonPressedImpl(MouseCode button) {
         auto window = static_cast<GLFWwindow *>(Application::Get().GetWindow().getNativeWindow());
-        auto state = glfwGetMouseButton(window, button);
+		auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
 
         return state == GLFW_PRESS;
     }
